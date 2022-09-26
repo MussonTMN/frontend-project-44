0ba8d7bd8b642c972f20f8fbf0fa1882326eb8a7
musson <mussontmn@gmail.com>
2022-09-26 20:11:40 +0300

    fixed eslint errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import userName from '/home/musson/frontend-project-44/src/cli.js';
+import userName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 userName();
