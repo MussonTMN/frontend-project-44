e60f079057d0da1d35aec38e6572865afaefff33
musson <mussontmn@gmail.com>
2022-09-29 12:17:58 +0300

    added index.js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+// eslint-disable-next-line consistent-return
+console.log('Welcome to the Brain Games!');
+
+const name = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${name}!`);
+console.log(`${gameQuestion}`);
+
+const game = () => {
+  let i = 1;
+  while (i <= 3) {
+    console.log(`Question: ${task}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    const rightCondition = answer === trueResult;
+
+    if (rightCondition) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueResult()}'. \nLet's try again, ${name}!`);
+    }
+  } return console.log(`Congratulations, ${name}!`);
+};
