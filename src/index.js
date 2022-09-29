903c74a77112d6f3eea74d331b3e8a2f1cc18911
musson <mussontmn@gmail.com>
2022-09-29 16:10:24 +0300

    finally it works

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line consistent-return
-console.log('Welcome to the Brain Games!');
+export default (gameQuestion, trueResult, task) => {
+  console.log('Welcome to the Brain Games!');
 
-const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
 
-console.log(`Hello, ${name}!`);
-console.log(`${gameQuestion}`);
+  console.log(`Hello, ${name}!`);
+  console.log(`${gameQuestion}`);
 
-const game = () => {
-  let i = 1;
-  while (i <= 3) {
-    console.log(`Question: ${task}`);
+  const game = () => {
+    let i = 1;
+    while (i <= 3) {
+      console.log(`Question: ${task}`);
 
-    const answer = readlineSync.question('Your answer: ');
+      const answer = readlineSync.question('Your answer: ');
 
-    const rightCondition = answer === trueResult;
+      const rightCondition = Number(answer) === trueResult();
 
-    if (rightCondition) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueResult()}'. \nLet's try again, ${name}!`);
-    }
-  } return console.log(`Congratulations, ${name}!`);
+      if (rightCondition) {
+        console.log('Correct!');
+        i += 1;
+      } else {
+        return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueResult()}'. \nLet's try again, ${name}!`);
+      }
+    } return console.log(`Congratulations, ${name}!`);
+  };
+  game();
 };
