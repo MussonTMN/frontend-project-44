5c83a68477d97a1b27a52415c38c6ad3ba38560d
musson <mussontmn@gmail.com>
2022-09-29 12:18:15 +0300

    added brain-calc

---
+++ games/calc.js
@@ -0,0 +1,38 @@
+import getRandomNum from '../src/getRandomNum.js';
+import calcGame from '../src/even.js';
+
+const gameQuestion = 'What is the result of the expression?';
+
+const randomInteger = (min, max) => {
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+};
+
+const oneOfThree = () => {
+  const items = ['+', '-', '*'];
+  return items[randomInteger(0, 2)];
+};
+
+const firstNum = getRandomNum();
+const secondNum = getRandomNum();
+
+const task = `${firstNum} ${oneOfThree()} ${secondNum}`;
+
+const trueResult = () => {
+  const taskColl = task.split(' ');
+  const first = Number(taskColl[0]);
+  const second = Number(taskColl[2]);
+  const operator = taskColl[1];
+  let result = 0;
+  if (operator === '+') {
+    result = first + second;
+  } else if (operator === '-') {
+    result = first - second;
+  } else if (operator === '*') {
+    result = first * second;
+  } return result;
+};
+
+export {
+  gameQuestion, task, trueResult, calcGame,
+};
