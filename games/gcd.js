20982183381e2d2cf248d57c0b4ebb4e1d58b9db
musson <mussontmn@gmail.com>
2022-09-30 19:10:29 +0300

    fixed trueResult

--- games/gcd.js
+++ games/gcd.js
@@ -4,6 +4,13 @@ import gcdGame from '../src/index.js';
 const gameQuestion = 'Find the greatest common divisor of given numbers.';
 
 const trueResult = (first, second) => {
+  const lesserOperand = first <= second ? first : second;
+  const biggerOperand = first >= second ? first : second;
+  let gcd = lesserOperand;
+  while (lesserOperand % gcd !== 0 || biggerOperand % gcd !== 0) {
+    gcd -= 1;
+  }
+  return String(gcd);
 };
 
 const data = () => {
