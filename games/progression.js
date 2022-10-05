805b827bdc56079da1adcbd4841d753892bee672
musson <mussontmn@gmail.com>
2022-10-05 12:27:50 +0300

    renamed functions, made getProgression clear, 10th

--- games/progression.js
+++ games/progression.js
@@ -3,20 +3,20 @@ import progressionGame from '../src/index.js';
 
 const gameQuestion = 'What number is missing in the progression?';
 
-const getRandomArray = () => {
-  const lengthOfArray = getRandomNum(5, 15);
-  const first = getRandomNum(0, 100);
-  const difference = getRandomNum(1, 9);
-  const coll = [first];
+const getProgression = (firstElement, lengthOfArray, numberStep) => {
+  const coll = [firstElement];
   for (let i = 1; i < lengthOfArray; i += 1) {
-    const nextElement = coll[i - 1] + difference;
+    const nextElement = coll[i - 1] + numberStep;
     coll.push(nextElement);
   }
   return coll;
 };
 
 const data = () => {
-  const array = getRandomArray();
+  const lengthOfArray = getRandomNum(5, 15);
+  const firstElement = getRandomNum(0, 100);
+  const numberStep = getRandomNum(1, 9);
+  const array = getProgression(firstElement, lengthOfArray, numberStep);
   const arrayLength = array.length - 1;
   const getMissingNum = getRandomNum(0, arrayLength);
   const arrayWithMiss = array.splice(getMissingNum, 1, '..');
