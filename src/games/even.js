6c857df356d15007ad2d5e12a887ddb2f2f10b89
musson <mussontmn@gmail.com>
2022-10-05 16:59:07 +0300

    again

--- src/games/even.js
+++ src/games/even.js
@@ -7,7 +7,7 @@ export default () => {
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
-  console.log("Answer 'yes' if number is even, otherwise answer 'no'.");
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   let i = 1;
   while (i <= 3) {
