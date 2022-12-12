5c0727b3e79341c2e47257a502f5c2ad01eec87b
musson <mussontmn@gmail.com>
2022-12-12 15:35:29 +0300

    fix even

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,11 @@ import getRandomNum from '../utils.js';
 import evenGame from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const getNumber = getRandomNum(0, 100);
 
 const isEven = (num) => num % 2 === 0;
 
 const getTaskAndSolution = () => {
-  const getNumber = getRandomNum(0, 100);
   const expectedResult = isEven(getNumber) ? 'yes' : 'no';
   const task = `${getNumber}`;
   return [task, expectedResult];
